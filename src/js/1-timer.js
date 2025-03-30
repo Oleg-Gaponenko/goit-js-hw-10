3a8654bbd10e304b7a51a050c3e6b23be8e0b768
Oleg Gaponenko <oleggaponenko@gmail.com>
2025-03-30 19:46:59 +0300

    modify 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,14 +1,36 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
+    onClose: handleOnClose,
   };
 
-  const fp = flatpickr("datetime-picker", {});
+
+const newInput = document.querySelector('#datetime-picker');
+flatpickr(newInput, options);
+
+let userSelectedDate = [];
+const startButton = document.querySelector('.button-start');
+
+function handleOnClose (selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if(selectedDate <= currentDate){
+        iziToast.info({
+            title: 'Info',
+            message: 'Please choose a date in the future',
+        });
+        startButton.disabled = true;
+    } else {
+        userSelectedDate = selectedDate;
+        startButton.disabled = false;
+    } 
+}
+
