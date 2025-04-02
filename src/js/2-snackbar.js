7ea508ef315fd5a8c9ddd210b7799714193eee0e
Oleg Gaponenko <oleggaponenko@gmail.com>
2025-04-03 00:22:15 +0300

    modify task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,35 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
+const radioButton = document.querySelector('.state-input');
+const delayInput = document.querySelector('.delay-input-form');
+
+function handleSubmit(event){
+    event.preventDefault();
+
+    const delay = event.elements.delay.value;
+    const checkboxState = event.elements.state.value;
+    
+    const promiseGenerator = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        if(checkboxState === 'fulfilled'){
+            resolve(
+                iziToast.success({
+                Message: `Fulfilled promise in ${delay}ms`
+            }));
+        } else {
+            reject(
+                iziToast.error({
+                    message: `Rejected promise in ${delay}ms`
+                })
+            )
+        }
+    }, delay)
+});
+
+    promiseGenerator
+        .then(value => console.log(value))
+        .catch(error => console.log(error))
+}
